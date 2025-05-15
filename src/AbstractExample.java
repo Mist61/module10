0f7420d6e94bf81a60d7c85aa0bcde4c3e8d6904
Mist61 <87336861+Mist61@users.noreply.github.com>
2025-05-15 01:58:41 -0400

    new

---
+++ src/AbstractExample.java
@@ -0,0 +1,32 @@
+abstract class Animal {
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    abstract void makeSound();
+
+    void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
+}
+
+class Dog extends Animal {
+    Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " says: Woof!");
+    }
+}
+
+public class AbstractExample {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy");
+        dog.makeSound();
+        dog.sleep();
+    }
+}
